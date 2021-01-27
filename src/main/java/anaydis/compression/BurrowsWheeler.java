9a066c7761e9c8d8ed87157b8c774de08bae1d70
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2021-01-27 19:53:07 -0300

    BW decode works in tests

--- src/main/java/anaydis/compression/BurrowsWheeler.java
+++ src/main/java/anaydis/compression/BurrowsWheeler.java
@@ -9,55 +9,63 @@ public class BurrowsWheeler implements anaydis.compression.Compressor {
     public void encode(@NotNull InputStream inputStream, @NotNull OutputStream outputStream) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         String str = br.readLine();
+        str+="$";
             String[] strs = new String[str.length()];
             for (int i = 0; i < strs.length; ++i) {
                 strs[i] = str.substring(i) + str.substring(0, i);
+                System.out.println(strs[i]);
             }
-
+            //sorts the array in ascending order
             java.util.Arrays.sort(strs);
 
 
             for (int i = 0; i < strs.length; ++i) {
+//                System.out.println(strs[i].charAt(strs[i].length() - 1));
                 outputStream.write(strs[i].charAt(strs[i].length() - 1));
             }
 
     }
-
+//ANNB$AA
 
     @Override
     public void decode(@NotNull InputStream inputStream, @NotNull OutputStream outputStream) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         String str = br.readLine();
 
-            Integer[] indices = new Integer[str.length()];
-            for (int i = 0; i < indices.length; ++i) {
-                indices[i] = i;
+        Integer[] indexes = new Integer[str.length()-1];
+        int firstCharPosition = 0;
+        for (int i = 0; i < indexes.length; ++i) {
+            indexes[i] = i;
+            if(str.charAt(i) == '$'){
+                firstCharPosition = i-1;
+                StringBuilder sb = new StringBuilder(str);
+                sb.deleteCharAt(i);
+                str = (String)sb.toString();
             }
-            java.util.Arrays.sort(indices, new BWComparator(str));
+        }
 
-            for (int i = 0; i < indices.length; ++i) {
-                if(str.charAt(i)!='$') outputStream.write(str.charAt(i));
+
+        char[] sorted = str.toCharArray();
+        java.util.Arrays.sort(sorted);
+
+        int firstPositionSorted = 0;
+        for (int i = 0; i < sorted.length; i++) {
+            if(sorted[i] == str.charAt(firstCharPosition))firstPositionSorted = i;
+        }
+
+        for (int i = firstCharPosition; i >0; i--) {
+                outputStream.write(str.charAt(i));
+//                System.out.println(str.charAt(i));
+            for (int j = firstPositionSorted-1; j >0; j--) {
+                outputStream.write(sorted[j]);
+//                System.out.println(sorted[j]);
+                break;
             }
-
+        }
 
 
 
     }
 
-    private static final class BWComparator implements java.util.Comparator<Integer> {
-        private final String string;
 
-        BWComparator(String string) {
-            this.string = string;
-        }
-
-        @Override
-        public int compare(Integer i, Integer j) {
-            return string.charAt(i) - string.charAt(j);
-        }
-
-        public boolean equals(Integer i, Integer j) {
-            return string.charAt(i) == string.charAt(j);
-        }
-    }
 }
