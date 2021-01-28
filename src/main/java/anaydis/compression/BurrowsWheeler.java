c16727ab7f3c5dc89d1e5b97cba9f9c6346d7fcf
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2021-01-27 22:28:04 -0300

    Changes in encoding and decoding BWT

--- src/main/java/anaydis/compression/BurrowsWheeler.java
+++ src/main/java/anaydis/compression/BurrowsWheeler.java
@@ -7,61 +7,101 @@ import java.io.*;
 public class BurrowsWheeler implements anaydis.compression.Compressor {
     @Override
     public void encode(@NotNull InputStream inputStream, @NotNull OutputStream outputStream) throws IOException {
+        //paso el input a string
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         String str = br.readLine();
-        str+="$";
+//        str+="$";
+
+        //genero el array de strings con todas las rotaciones
             String[] strs = new String[str.length()];
             for (int i = 0; i < strs.length; ++i) {
                 strs[i] = str.substring(i) + str.substring(0, i);
-                System.out.println(strs[i]);
             }
-            //sorts the array in ascending order
+            //los ordeno de manera que sus iniciales esten en orden alfabetico creciente
             java.util.Arrays.sort(strs);
 
 
+            //genero el output (String formado por el ultimo char de cada string del arreglo + la posicion del primer char del input original)
+            int index = 0;
             for (int i = 0; i < strs.length; ++i) {
-//                System.out.println(strs[i].charAt(strs[i].length() - 1));
                 outputStream.write(strs[i].charAt(strs[i].length() - 1));
+                if(strs[i].charAt(strs[i].length() - 1) == str.charAt(0)) index = i;
             }
+             outputStream.write(Character.forDigit(index, 10));
+
 
     }
 //ANNB$AA
 
     @Override
     public void decode(@NotNull InputStream inputStream, @NotNull OutputStream outputStream) throws IOException {
+        //paso el input a string
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         String str = br.readLine();
 
-        Integer[] indexes = new Integer[str.length()-1];
-        int firstCharPosition = 0;
-        for (int i = 0; i < indexes.length; ++i) {
-            indexes[i] = i;
-            if(str.charAt(i) == '$'){
-                firstCharPosition = i-1;
-                StringBuilder sb = new StringBuilder(str);
-                sb.deleteCharAt(i);
-                str = (String)sb.toString();
-            }
-        }
+        //busco la posicion del primer indice y lo borro del input (str es mi L)
+        int firstCharPosition = Character.getNumericValue(str.charAt(str.length()-1));
+        StringBuilder sb = new StringBuilder(str);
+        sb.deleteCharAt(str.length()-1);
+        str = (String)sb.toString();
 
 
+        //ordeno L para obtener F
         char[] sorted = str.toCharArray();
         java.util.Arrays.sort(sorted);
 
-        int firstPositionSorted = 0;
-        for (int i = 0; i < sorted.length; i++) {
-            if(sorted[i] == str.charAt(firstCharPosition))firstPositionSorted = i;
+        //creo un array de integer(vector de transformacion)
+        Integer[] indexes = new Integer[sorted.length];
+        for (int i = 0; i < indexes.length; i++) {
+            indexes[i] = i;
         }
 
-        for (int i = firstCharPosition; i >0; i--) {
-                outputStream.write(str.charAt(i));
-//                System.out.println(str.charAt(i));
-            for (int j = firstPositionSorted-1; j >0; j--) {
-                outputStream.write(sorted[j]);
-//                System.out.println(sorted[j]);
-                break;
+
+        //ordeno el vector de transformacion
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < sorted.length; j++) {
+                if(str.charAt(i) == sorted[j]) {
+                    indexes[j] = i;
+                    sorted[j] = 'å';
+                    break;
+                }
             }
         }
+
+
+
+        for (int i = 0; i < indexes.length; i++) {
+            System.out.println(indexes[i]);
+        }
+
+//
+        int index = firstCharPosition;
+        for ( int i = 0 ; i < indexes.length; i++ ) {
+            outputStream.write(str.charAt(index));
+            index = indexes[index];
+        }
+
+
+
+
+
+
+
+//
+//        int firstPositionSorted = 0;
+//        for (int i = 0; i < sorted.length; i++) {
+//            if(sorted[i] == str.charAt(firstCharPosition))firstPositionSorted = i;
+//        }
+//
+//        for (int i = firstCharPosition; i >0; i--) {
+//                outputStream.write(str.charAt(i));
+////                System.out.println(str.charAt(i));
+//            for (int j = firstPositionSorted-1; j >0; j--) {
+//                outputStream.write(sorted[j]);
+////                System.out.println(sorted[j]);
+//                break;
+//            }
+//        }
 
 
 
--- src/test/java/anaydis/compression/BurrowsWheelerTester.java
+++ src/test/java/anaydis/compression/BurrowsWheelerTester.java
@@ -12,13 +12,13 @@ public class BurrowsWheelerTester {
 
     @Test
     public void testEncode() throws IOException {
-//
+
 //        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
 //        InputStream inputStream = new FileInputStream(file);
 //        OutputStream outputStream = new FileOutputStream(file);
 //        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-////        outputStream.write("97, 97, 97, 97, 115, 115, 97, 97, 97, 97, 97, 97, 97, 97, 115, 32, 32, 32, 109, 109, 109, 97, 97, 97, 97, 97, 97".getBytes());
-//        outputStream.write("BANANA".getBytes());
+//        outputStream.write("DRDOBBS".getBytes());
+////        outputStream.write("BANANA".getBytes());
 //        burrowsWheeler.encode(inputStream, outputStream);
 //
 //        StringBuilder stringBuilder = new StringBuilder();
@@ -26,28 +26,30 @@ public class BurrowsWheelerTester {
 //        System.out.println(str);
 //
 ////        Assert.assertEquals("NNBAAA", str);
-
+//
 
     }
 
     @Test
     public void testDecode() throws IOException {
-//
+
 //        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
 //        InputStream inputStream = new FileInputStream(file);
 //        OutputStream outputStream = new FileOutputStream(file);
 //        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-////        outputStream.write(",,,,,,,,,,,,,,,,,,,,,,,,,,29977522577777777775977777111      111333   111999999999999999999000      7 ".getBytes());
-//        outputStream.write("ANNB$AA".getBytes());
+
+
+//        outputStream.write("OBRSDDB5".getBytes());
+////        outputStream.write("ANNB$AA".getBytes());
 //        burrowsWheeler.decode(inputStream, outputStream);
 //
 //        StringBuilder stringBuilder = new StringBuilder();
 //        String str = br.readLine();
 //        System.out.println(str);
+
+//        Assert.assertEquals("BANANA", str);
 //
-////        Assert.assertEquals("BANANA", str);
-//
-//
+
 
     }
 }
