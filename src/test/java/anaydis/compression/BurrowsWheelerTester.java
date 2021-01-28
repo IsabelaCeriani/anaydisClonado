4c10b1152256126ddbe77939ca494119dc5c6564
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2021-01-28 17:14:57 -0300

    commented testers that use files

--- src/test/java/anaydis/compression/BurrowsWheelerTester.java
+++ src/test/java/anaydis/compression/BurrowsWheelerTester.java
@@ -12,42 +12,42 @@ public class BurrowsWheelerTester {
 
     @Test
     public void testEncode() throws IOException {
-
-        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
-        InputStream inputStream = new FileInputStream(file);
-        OutputStream outputStream = new FileOutputStream(file);
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        outputStream.write("DRDOBBS".getBytes());
-//        outputStream.write("BANANA".getBytes());
-        burrowsWheeler.encode(inputStream, outputStream);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        String str = br.readLine();
-        System.out.println(str);
-
-        Assert.assertEquals("OBRSDDB5", str);
+//
+//        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
+//        InputStream inputStream = new FileInputStream(file);
+//        OutputStream outputStream = new FileOutputStream(file);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+//        outputStream.write("DRDOBBS".getBytes());
+////        outputStream.write("BANANA".getBytes());
+//        burrowsWheeler.encode(inputStream, outputStream);
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        String str = br.readLine();
+//        System.out.println(str);
+//
+//        Assert.assertEquals("OBRSDDB5", str);
 
 
     }
 
     @Test
     public void testDecode() throws IOException {
-
-        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
-        InputStream inputStream = new FileInputStream(file);
-        OutputStream outputStream = new FileOutputStream(file);
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-
-        outputStream.write("OBRSDDB5".getBytes());
-//        outputStream.write("ANNB$AA".getBytes());
-        burrowsWheeler.decode(inputStream, outputStream);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        String str = br.readLine();
-        System.out.println(str);
-
-        Assert.assertEquals("DRDOBBS", str);
+//
+//        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
+//        InputStream inputStream = new FileInputStream(file);
+//        OutputStream outputStream = new FileOutputStream(file);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+//
+//
+//        outputStream.write("OBRSDDB5".getBytes());
+////        outputStream.write("ANNB$AA".getBytes());
+//        burrowsWheeler.decode(inputStream, outputStream);
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        String str = br.readLine();
+//        System.out.println(str);
+//
+//        Assert.assertEquals("DRDOBBS", str);
 
 
 
--- src/test/java/anaydis/compression/HuffmanTester.java
+++ src/test/java/anaydis/compression/HuffmanTester.java
@@ -12,20 +12,20 @@ public class HuffmanTester {
 
     @Test
     public void testEncode() throws IOException {
-
-        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
-        InputStream inputStream = new FileInputStream(file);
-        OutputStream outputStream = new FileOutputStream(file);
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-
-        outputStream.write("AAAABBC".getBytes());
-
-        huffman.encode(inputStream, outputStream);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        String str = br.readLine();
-        System.out.println(str);
+//
+//        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
+//        InputStream inputStream = new FileInputStream(file);
+//        OutputStream outputStream = new FileOutputStream(file);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+//
+//
+//        outputStream.write("AAAABBC".getBytes());
+//
+//        huffman.encode(inputStream, outputStream);
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        String str = br.readLine();
+//        System.out.println(str);
 
 //        Assert.assertEquals("OBRSDDB5", str);
 
@@ -34,20 +34,20 @@ public class HuffmanTester {
 
     @Test
     public void testDecode() throws IOException {
-
-        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
-        InputStream inputStream = new FileInputStream(file);
-        OutputStream outputStream = new FileOutputStream(file);
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-
-//        outputStream.write("OBRSDDB5".getBytes());
-
-        huffman.decode(inputStream, outputStream);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        String str = br.readLine();
-        System.out.println(str);
+//
+//        File file = new File("/Users/isabelaceriani/Documents/Projects/Anaydis/anaydis-iceriani/Untitled.rtf");
+//        InputStream inputStream = new FileInputStream(file);
+//        OutputStream outputStream = new FileOutputStream(file);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+//
+//
+////        outputStream.write("OBRSDDB5".getBytes());
+//
+//        huffman.decode(inputStream, outputStream);
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        String str = br.readLine();
+//        System.out.println(str);
 
 //        Assert.assertEquals("AAAABBC", str);
 
