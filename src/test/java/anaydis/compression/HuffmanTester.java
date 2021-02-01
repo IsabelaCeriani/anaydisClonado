a37c8506e4441d49378c603115f3ff292ac4b889
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2021-02-01 14:01:05 -0300

    Commented huffman class and tester

--- src/test/java/anaydis/compression/HuffmanTester.java
+++ src/test/java/anaydis/compression/HuffmanTester.java
@@ -9,50 +9,50 @@ public class HuffmanTester {
 
     Huffman huffman = new Huffman();
 
-
-    @Test
-    public void testEncode() throws IOException {
-
-
-        String str = "AAAAAAAAAAABBBBBBBC";
-        InputStream inputStream = new ByteArrayInputStream(str.getBytes());
-        OutputStream outputStream = new ByteArrayOutputStream();
-
-
-
-        huffman.encode(inputStream, outputStream);
-
-        String result = outputStream.toString();
-        Assert.assertEquals("111111111110101010101010100", result);
-
-
-    }
-
-    @Test
-    public void testDecode() throws IOException {
-        String str = "AAAAAAAAAAABBBBBBBC";
-        InputStream inputStream = new ByteArrayInputStream(str.getBytes());
-        OutputStream outputStream = new ByteArrayOutputStream();
-
-
-
-        huffman.encode(inputStream, outputStream);
-
-        String result = outputStream.toString();
-
-        InputStream newInputStream = new ByteArrayInputStream(outputStream.toString().getBytes());
-        OutputStream newoutputStream = new ByteArrayOutputStream();
-
-
-        huffman.decode(newInputStream, newoutputStream);
-
-
-        result = newoutputStream.toString();
-
-
-
-
-
-    }
+//
+//    @Test
+//    public void testEncode() throws IOException {
+//
+//
+//        String str = "AAAAAAAAAAABBBBBBBC";
+//        InputStream inputStream = new ByteArrayInputStream(str.getBytes());
+//        OutputStream outputStream = new ByteArrayOutputStream();
+//
+//
+//
+//        huffman.encode(inputStream, outputStream);
+//
+//        String result = outputStream.toString();
+//        Assert.assertEquals("111111111110101010101010100", result);
+//
+//
+//    }
+//
+//    @Test
+//    public void testDecode() throws IOException {
+//        String str = "AAAAAAAAAAABBBBBBBC";
+//        InputStream inputStream = new ByteArrayInputStream(str.getBytes());
+//        OutputStream outputStream = new ByteArrayOutputStream();
+//
+//
+//
+//        huffman.encode(inputStream, outputStream);
+//
+//        String result = outputStream.toString();
+//
+//        InputStream newInputStream = new ByteArrayInputStream(outputStream.toString().getBytes());
+//        OutputStream newoutputStream = new ByteArrayOutputStream();
+//
+//
+//        huffman.decode(newInputStream, newoutputStream);
+//
+//
+//        result = newoutputStream.toString();
+//
+//
+//
+//
+//
+//    }
 }
 
